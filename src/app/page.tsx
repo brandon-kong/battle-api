71aa4b1a9f4b39e348aa39170e9ae932a4885239
brandon-kong <kongbrandon0@gmail.com>
2024-02-13 21:33:54 -0600

    Padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,22 @@ export default function Home() {
     return (
         <main className="flex place-content-center items-center bg-gradient-to-t from-blue-100 to-blue-50 pt-navbar-2x xl:pt-navbar">
             <div
-            className={'max-w-content-xs md:max-w-content px-content-padding-x h-min xl:h-[720px] w-full flex flex-col place-content-end'}
+            className={'max-w-content-xs md:max-w-content px-content-padding-x xl:px-0 h-min xl:h-[720px] w-full flex flex-col place-content-end'}
             >
                 
                 <div
-                className={'flex gap-8 flex-col md:flex-col-reverse xl:flex-row-reverse w-full place-content-between md:place-items-start'}
+                className={'flex gap-12 xl:gap-8 flex-col md:flex-col-reverse xl:flex-row-reverse w-full place-content-between md:place-items-start'}
                 >
 
                     <div
-                    className={'flex flex-col md:flex-row xl:flex-col gap-6 xl:max-w-xl'}
+                    className={'flex flex-col md:flex-row xl:flex-col gap-6 md:gap-12 xl:gap-6 xl:max-w-xl'}
                     >
                         <Typography variant="h1" className={'md:flex-[.5] w-full xl:w-full'}>
-                            battle-testing your application has never been <span className={'text-blue-600'}>easier.</span>
+                            battle testing your software has never been <span className={'text-blue-600'}>easier.</span>
                         </Typography>
 
                         <div className={'flex flex-col space-y-6 md:flex-[.5] w-full xl:w-full'}>
-                            <Typography variant="p-large" className={'max-w-xl'}>
+                            <Typography variant="p-large" className={''}>
                                 Get paid for your business — online, in-store, or on-the-go. Platform thousands of businesses trust.
                             </Typography>   
                             
